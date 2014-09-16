b0cf7b00afe653a7dcf40caa8f9b06ec21251299
Sourav Basu <devels17@gmail.com>
2014-09-17 00:47:19 +0530

    Current Valuation printing

---
+++ bittracker.py
@@ -0,0 +1,18 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+url = "http://www.coindesk.com/price/" 
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+req = urllib2.Request(url,headers=hdr)
+soup = BeautifulSoup(urllib2.urlopen(req))
+def getBitValue(soup):
+	# get data div for BitCoin Value
+	div = soup.find(name="div",attrs = {'class':'bpi-value bpiUSD'}).text
+	print "CurrentValuation: ",div
+	# get down change div
+	down_div = soup.find(name="div", attrs={'class': 'percent data-down'})
+	print "-",down_div,"%"
+
+getBitValue(soup)
+	
